66e2b9ea7ec7f0b78281d3cdddd5faa6aee29f49
jessica <jessicabispo.work@gmail.com>
2022-06-02 01:12:38 -0300

    mudanças em todo o html, iniciando processo de estilização com paleta definida

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//redirecionando botoes da pagina inicial 
+
+let home = $('#home').on('click', ()=>{
+    location.assign("seila.html")
+})
+
+
+let conta = $('#minhaConta').on('click', ()=>{
+    location.assign("seila.html")
+})
+
+let planos = $('#planos').on('click', ()=>{
+    location.assign("seila.html")
+})
+
+let sobre = $('#sobre').on('click', ()=>{
+    location.assign("seila.html")
+})
+
+let login = $('#login').on('click', ()=>{
+    location.assign("seila.html")
+})
+
+let assine = $('#assine').on('click', ()=>{
+    location.assign("seila.html")
+})
+
+let assineBtn = $('#assineBtn').on('click', ()=>{
+    location.assign("seila.html")
+})
