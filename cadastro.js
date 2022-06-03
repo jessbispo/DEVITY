cfe6bfdcce467749f049d1e5485f26afab8eb1e7
Isadora Áraújo <isadoraraujo13@gmail.com>
2022-06-03 12:03:49 -0300

    implementação da verificação da confirmação de email e senha

--- cadastro.js
+++ cadastro.js
@@ -4,5 +4,26 @@ let form = $('#form').submit((event) => {
 })
 
 function cadastro() {
+    function comparaEmail() {
+        let email = $('#email').val()
+        let confirmaEmail = $('#confirmaEmail').val()
+    
+        if (email !== confirmaEmail) {
+            alert('Os endereços de email precisam ser estritamente iguais.')
+        }
+    }
+    comparaEmail()
+    
+    function comparaSenha() {
+        let senha = $('#senha').val()
+        let confirmaSenha = $('#confirmaSenha').val()
+        
+        if (senha !== confirmaSenha) {
+            alert('As senhas digitadas não estão idênticas')
+        }
+    }
+    comparaSenha()
 
+    //RG
+    //CPF
 }
