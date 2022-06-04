276cd31483d45520cc05317ed94f307fbd20d650
Isadora Áraújo <isadoraraujo13@gmail.com>
2022-06-04 14:25:03 -0300

    consumo da API de CEP

--- cadastro.js
+++ cadastro.js
@@ -12,28 +12,46 @@ function cadastro() {
 
         if (email !== confirmaEmail) {
             $('#camposIguais').removeClass('d-none')
-            setTimeout(setAlert, 2000)      
+            setTimeout(setAlert, 2000)
         }
         if (senha !== confirmaSenha) {
             $('#camposIguais').removeClass('d-none')
-            setTimeout(setAlert, 2000)      
+            setTimeout(setAlert, 2000)
         }
-    } 
+    }
     comparaEmailESenha()
-    
-    function senhaForte() {
-        let senha = $('#senha').val()
-        if (senha.length !== null && senha.length <= 7) {
-           $('#senhaForte').removeClass('d-none') 
-           setTimeout(setAlert, 2000)       
-        } 
-    } setTimeout(senhaForte, 4000) 
+}//apagar 
+
+$(document).ready(function () {
+    $('#cep').blur(function () {
+        let cepDigitado = $(this).val()
+        $.get('https://viacep.com.br/ws/' + cepDigitado + '/json/', function (dados, status) {
+            if (status === success) {
+                $('#pais').val('Brasil')
+                $('#uf').val(dados.uf)
+                $('#cidade').val(dados.localidade)
+                $('#bairro').val(dados.bairro)
+                $('#rua').val(dados.logradouro)
+            } else {
+                alert('Digite um CEP válido')
+            }
+        })
+    })
+})
+
+/*function senhaForte() {
+    let senha = $('#senha').val()
+    if (senha.length !== null || senha.length <= 7) {
+       $('#senhaForte').removeClass('d-none')
+       setTimeout(setAlert, 2000)
+    }
+} senhaForte()
 }
 
 function setAlert(){
-    $('#senhaForte').addClass('d-none')     
-    $('#camposIguais').addClass('d-none')
-}
+$('#senhaForte').addClass('d-none')
+$('#camposIguais').addClass('d-none')
+}*/
 
 //RG
 //CPF
