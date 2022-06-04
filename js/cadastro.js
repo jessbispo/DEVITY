c072301d502afa7a8a0f3bcfcacd4b4219874d01
Isadora Áraújo <isadoraraujo13@gmail.com>
2022-06-04 16:24:57 -0300

    formato rg e cpf

--- js/cadastro.js
+++ js/cadastro.js
@@ -19,22 +19,33 @@ function cadastro() {
             setTimeout(setAlert, 2000)
         }
     }
-    comparaEmailESenha()
-}//apagar 
+    comparaEmailESenha()   
+}
+
+$(document).ready(function() {
+    let cpf = $('#cpf')
+    cpf.mask('000.000.000-00')
+
+    let rg = $('#rg')
+    rg.mask('00.000.000-0')
+})
+
+
 
 $(document).ready(function () {
     $('#cep').blur(function () {
         let cepDigitado = $(this).val()
         $.get('https://viacep.com.br/ws/' + cepDigitado + '/json/', function (dados, status) {
-            if (status === success) {
-                $('#pais').val('Brasil')
-                $('#uf').val(dados.uf)
-                $('#cidade').val(dados.localidade)
-                $('#bairro').val(dados.bairro)
-                $('#rua').val(dados.logradouro)
-            } else {
-                alert('Digite um CEP válido')
-            }
+            if (status === 'success') {
+            $('#pais').val('Brasil').attr('readonly', 'true').attr('disabled', 'true')
+            $('#uf').val(dados.uf).attr('readonly', 'true').attr('disabled', 'true')
+            $('#cidade').val(dados.localidade).attr('readonly', 'true').attr('disabled', 'true')
+            $('#bairro').val(dados.bairro).attr('readonly', 'true').attr('disabled', 'true')
+            $('#rua').val(dados.logradouro).attr('readonly', 'true').attr('disabled', 'true')
+        } // falta um else if, para checar se o cep existe
+        else {
+            alert('Ocorreu um problema na sua requisição. Por favor, tente novamente.')
+        }
         })
     })
 })
@@ -53,5 +64,7 @@ $('#senhaForte').addClass('d-none')
 $('#camposIguais').addClass('d-none')
 }*/
 
-//RG
-//CPF
+//TODOS os alerts com problema
+//tratativa de erro de requisição
+//trativa de erro se algum campo for 0 
+//finalização (função que coloca tudo junto e manda alert de cadastrado)
