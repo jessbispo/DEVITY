74495abaab33429d39bb6d68e690a7a7c2eb9d38
jessica <jessicabispo.work@gmail.com>
2022-06-04 17:41:27 -0300

    deletando imagens e doc html

---
+++ js/index.js
@@ -0,0 +1,26 @@
+//redirecionando botoes da pagina inicial 
+
+let logo = $('#logo').on('click', ()=>{
+    location.assign("seila.html")
+})
+
+let planos = $('#planos').on('click', ()=>{
+    location.assign("seila.html")
+})
+
+let sobre = $('#sobre').on('click', ()=>{
+    location.assign("seila.html")
+})
+
+let login = $('#login').on('click', ()=>{
+    location.assign("seila.html")
+})
+
+let assine = $('#assine').on('click', ()=>{
+    location.assign("seila.html")
+})
+
+let assineBtn = $('#assineBtn').on('click', ()=>{
+    location.assign("seila.html")
+})
+
