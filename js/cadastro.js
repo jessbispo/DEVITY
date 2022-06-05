45e0e0dc02835a1ea5ffe5761ec03e024a228684
Isadora Áraújo <isadoraraujo13@gmail.com>
2022-06-05 16:12:37 -0300

    bugs corrigidos

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,5 +1,6 @@
 let form = $('#form').submit((event) => {
     event.preventDefault()
+    $('#alertSubmit').removeClass('d-none')
     return cadastro()
 })
 
@@ -11,18 +12,28 @@ function cadastro() {
         let confirmaSenha = $('#confirmaSenha').val()
 
         if (email !== confirmaEmail) {
-            $('#camposIguais').removeClass('d-none')
+            $('#camposIguaisEmail').removeClass('d-none')
             setTimeout(setAlert, 2000)
         }
         if (senha !== confirmaSenha) {
-            $('#camposIguais').removeClass('d-none')
+            $('#camposIguaisSenha').removeClass('d-none')
             setTimeout(setAlert, 2000)
         }
     }
-    comparaEmailESenha()   
+    comparaEmailESenha()
+    function senhaForte() {
+        let senha = $('#senha').val()
+        if (senha.length === 0) {
+            return
+        }
+        else if (senha.length <= 7) {
+           $('#senhaForte').removeClass('d-none')
+           setTimeout(setAlert, 2000)
+        }
+    } setTimeout(senhaForte,2000)
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     let cpf = $('#cpf')
     cpf.mask('000.000.000-00')
 
@@ -35,36 +46,31 @@ $(document).ready(function() {
 $(document).ready(function () {
     $('#cep').blur(function () {
         let cepDigitado = $(this).val()
+    
         $.get('https://viacep.com.br/ws/' + cepDigitado + '/json/', function (dados, status) {
-            if (status === 'success') {
-            $('#pais').val('Brasil').attr('readonly', 'true').attr('disabled', 'true')
-            $('#uf').val(dados.uf).attr('readonly', 'true').attr('disabled', 'true')
-            $('#cidade').val(dados.localidade).attr('readonly', 'true').attr('disabled', 'true')
-            $('#bairro').val(dados.bairro).attr('readonly', 'true').attr('disabled', 'true')
-            $('#rua').val(dados.logradouro).attr('readonly', 'true').attr('disabled', 'true')
-        } // falta um else if, para checar se o cep existe
-        else {
-            alert('Ocorreu um problema na sua requisição. Por favor, tente novamente.')
-        }
-        })
+            if(status === 'success') {
+                console.log(dados, status)
+                $('#pais').val('Brasil').attr('readonly', 'true').attr('disabled', 'true')
+                $('#uf').val(dados.uf).attr('readonly', 'true').attr('disabled', 'true')
+                $('#cidade').val(dados.localidade).attr('readonly', 'true').attr('disabled', 'true')
+                $('#bairro').val(dados.bairro).attr('readonly', 'true').attr('disabled', 'true')
+                $('#rua').val(dados.logradouro).attr('readonly', 'true').attr('disabled', 'true')
+            } 
+            else {
+                console.log('error')
+            }
+        })// else if (dados.)
+    
     })
 })
 
-/*function senhaForte() {
-    let senha = $('#senha').val()
-    if (senha.length !== null || senha.length <= 7) {
-       $('#senhaForte').removeClass('d-none')
-       setTimeout(setAlert, 2000)
-    }
-} senhaForte()
-}
-
 function setAlert(){
 $('#senhaForte').addClass('d-none')
-$('#camposIguais').addClass('d-none')
-}*/
+$('#camposIguaisEmail').addClass('d-none')
+$('#camposIguaisSenha').addClass('d-none')
+}
 
 //TODOS os alerts com problema
 //tratativa de erro de requisição
-//trativa de erro se algum campo for 0 
+//trativa de erro se algum campo for 0
 //finalização (função que coloca tudo junto e manda alert de cadastrado)
